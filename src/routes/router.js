c1f43bad077e0cc4488903a703c7873e3d0056e8
lggradosm <gabrielgradosmachado@gmail.com>
2023-11-02 18:11:21 -0500

    ra

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import  {createTicket, getTickets, nextTicket,clearList}  from '../controller/TicketController.js';
+import { setTicket } from '../services/TicketService.js';
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.get('/create',createTicket)
 router.get('/next',nextTicket)
 router.get('/clear',clearList)
 router.get('',getTickets)
-
+router.post('', setTicket)
 
 
 export default router;
