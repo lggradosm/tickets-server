a40557cc9160c18ab5d8ffee080a400bdd3b0157
lggradosm <gabrielgradosmachado@gmail.com>
2024-04-22 08:15:09 -0500

    final

--- src/conf/database.js
+++ src/conf/database.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 export const connect = () => {
-  const MONGO_DATABASE = "guerrasalas_db"
-  // const MONGO_HOST = "mongodb";
-    const MONGO_HOST = "localhost";
-
+  const MONGO_DATABASE = "guerrasalas_db";
+  const MONGO_HOST = "mongodb";
+  // const MONGO_HOST = "localhost";
   const MONGO_PORT = 27017;
 
   const MONGO_URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
-  
+
   mongoose
     .connect(MONGO_URI)
     .then((db) => console.log("Database is connected"))
